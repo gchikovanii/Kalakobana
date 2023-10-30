009a6d53942f89b0f19ea47eb2118b8c31b76ccc
gchikovanii <gchikovanii25@gmail.com>
2023-10-31 01:31:29 +0400

    CleanArchitecture Implemented

---
+++ Kalakobana/Kalakobana.Application/Countries/Requests/CountryRequestModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalakobana.Application.Countries.Requests
+{
+    public class CountryRequestModel
+    {
+        public int Id { get; set; }
+        public string CountryName { get; set; }
+    }
+}
---
+++ Kalakobana/Kalakobana.Infrastructure/Repositories/Countries/CountryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalakobana.Infrastructure.Repositories.Countries
+{
+    public class CountryRepository : ICountryRepository
+    {
+    }
+}
