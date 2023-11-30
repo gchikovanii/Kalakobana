1f32f5fd06300f94fc091e28277ea645afba213b
gchikovanii <gchikovanii25@gmail.com>
2023-11-30 16:10:28 +0400

    Update

--- Kalakobana/Kalakobana.Application/Account/LogoutUserCommandHandler.cs
+++ Kalakobana/Kalakobana.Application/Account/LogoutUserCommandHandler.cs
@@ -1,11 +1,5 @@
-﻿using Kalakobana.Application.Infrastructure.JWT;
-using MediatR;
+﻿using MediatR;
 using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Kalakobana.Application.Account
 {
